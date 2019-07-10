6da4a5b0b744e3a61d20849992e01d110e660cc5
Ian Cullinane <icullinane@turbine.com>
2019-07-09 20:40:24 -0400

    add subnet

--- generic-vpc.tf
+++ generic-vpc.tf
@@ -9,3 +9,39 @@ resource "aws_vpc" "generic" {
         Environment = "${var.environment}"
     }
 }
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+}
+
+/*
+  Public Subnet
+*/
+resource "aws_subnet" "us-east-2a-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    cidr_block = "${cidrsubnet(var.vpc_cidr, 4, 1)}"
+    availability_zone = "us-east-2a"
+
+    tags {
+        Name = "Public Subnet"
+    }
+}
+
+resource "aws_route_table" "us-east-2a-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+
+    tags {
+        Name = "Public Subnet"
+    }
+}
+
+resource "aws_route_table_association" "us-east-2a-public" {
+    subnet_id = "${aws_subnet.us-east-2a-public.id}"
+    route_table_id = "${aws_route_table.us-east-2a-public.id}"
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
     value = "${aws_vpc.generic.id}"
 }
+
+// output "vpc_id" {
+//     value = "${aws_vpc.generic.}"
+// }
